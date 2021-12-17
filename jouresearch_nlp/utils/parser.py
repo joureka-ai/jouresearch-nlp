8c5596ca9deb4a03797cfd6dd23f6b6e2a52f34e
FelixMertin <fmertineit@gmail.com>
2021-12-17 14:17:45 +0100

    [mod] add parsing from df to pydantic model for topic modelling

--- jouresearch_nlp/utils/parser.py
+++ jouresearch_nlp/utils/parser.py
@@ -1,8 +1,10 @@
 from . import nlp
+from ..topicmodelling import BERTopic
 from spacy.tokens import Token, Doc
 from typing import List, Any
-from jouresearch_nlp.schemas import WordCloudS, Word
-from multidict import MultiDict
+from jouresearch_nlp.schemas import WordCloudS, WordS
+from pandas import DataFrame
+from ..schemas import Topics, Topic, Word
 
 
 def tokenizer(text: str) -> Doc:
@@ -15,12 +17,24 @@ def lemma(token: Token) -> List[str]:
     return token.lemma_.lower()
 
 
+def remove_stopwords(text: List[str]) -> List[str]:
+    """Takes a list of strings and returns the words without stop_words"""
+    text_wo_sw = []
+    for tex in text:
+        tokens = tokenizer(tex)
+        token_wo_sw = [token for token in tokens if not token.is_stop]
+        tex_wo_sw = [token.text for token in token_wo_sw]
+        text_wo_sw.append(" ".join(tex_wo_sw))
+
+    return text_wo_sw
+
+
 def wc_parser(wc_layout: Any) -> WordCloudS:
     """Parses the output of the WordCloud layout data structure into the Pydantic WordCloud model."""
 
     wc_words = []
     for word in wc_layout:
-        wc_word = Word(
+        wc_word = WordS(
             word=word[0][0],
             frequency=word[0][1],
             font_size=word[1],
@@ -31,3 +45,40 @@ def wc_parser(wc_layout: Any) -> WordCloudS:
         wc_words.append(wc_word)
 
     return WordCloudS(words=wc_words)
+
+
+def tuple_to_dict(freqs: List[tuple]):
+    """Parse a tuple with (str, int) to a dictionary where the key the str is and the value the int."""
+    freq_dict = {}
+    for freq in freqs:
+        freq_dict[freq[0]] = freq[1]
+
+    return freq_dict
+
+
+def tm_parser(df: DataFrame, model: BERTopic) -> Topics:
+
+    topics = []
+    for row in df.iterrows():
+        topic_id = row[1]["Topic"]
+
+        freq_dict = tuple_to_dict(model.get_topic(topic_id))
+
+        words = [word for word in row[1]["Words"].split("|")]
+        words = [word.strip() for word in words]
+
+        t_word = []
+        for word in words:
+            t_w = Word(word=word, freq=freq_dict[word])
+            t_word.append(t_w)
+
+        topic = Topic(
+            x=row[1]["x"],
+            y=row[1]["y"],
+            label=topic_id,
+            words=t_word,
+            size=row[1]["Size"],
+        )
+        topics.append(topic)
+
+    return Topics(topics=topics)
