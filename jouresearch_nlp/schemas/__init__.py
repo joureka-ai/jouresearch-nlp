a6e43e3a0f5661426568db2dc42aea156151c956
FelixMertin <fmertineit@gmail.com>
2022-01-07 15:23:26 +0100

    [add] schemas and jupyter notebook for NER

--- jouresearch_nlp/schemas/__init__.py
+++ jouresearch_nlp/schemas/__init__.py
@@ -1,3 +1,4 @@
 from .wordcloud import WordCloudS, WordS
 from .document import Document
 from .topic import Topics, Topic, Word
+from .entity import NamedEntities, Entity, EntityLabel
--- jouresearch_nlp/schemas/entity.py
+++ jouresearch_nlp/schemas/entity.py
@@ -1,1 +1,17 @@
 from pydantic import BaseModel
+from typing import List
+
+
+class Entity(BaseModel):
+    name: str
+    frequency: int
+    recordings: List[int]
+
+
+class EntityLabel(BaseModel):
+    label: str
+    entities: List[Entity]
+
+
+class NamedEntities(BaseModel):
+    labelled_entities: List[EntityLabel]
